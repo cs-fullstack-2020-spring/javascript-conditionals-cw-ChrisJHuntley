9c42d43fa5a5f9a069887b24f68e289b755d7a51
ChrisJHuntley <cjhuntley4@gmail.com>
2020-02-17 08:09:00 -0600

    Final Push

---
+++ script.js
@@ -0,0 +1,30 @@
+let anyword=prompt("What is your first word?")
+let word=prompt("What is your second word? ")
+if(anyword==word){
+    console.log("THAT IS CORRECT")
+
+}
+if(anyword != word){
+    console.log("THAT IS INCORRECT")
+}
+
+let number= parseInt(prompt("What is your number?"));
+let another=0
+if(number >= another){
+    console.log("Your number is positive");
+}
+if(number<0){
+    console.log("Your number is negative");
+}
+
+ 
+
+let num1=prompt("Give a number?");
+let num2=prompt("Give another number?");
+ if(num1==num2){
+    console.log("Your number is even");
+}
+if(num1!=num2){
+    console.log("Your number is odd")
+}
+
